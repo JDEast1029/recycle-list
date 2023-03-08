c96628f7f8fadf947a7a668cd36f05cbd851464b
蒋东 <852778359@qq.com>
2023-03-08 17:54:35 +0800

    docs： 更新readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# recycle-list
+# RecycleList
+Vue3版本的无限滚动列表
+
+### Components
++ **`RecycleCore`**: 可容纳无限数据的滚动容器
++ **`PullDown`**: 提供下拉操作的容器组件
++ **`RecycleList`**: 支持上拉加载和下拉刷新，提供`default(item)`, `header`, `pull-down`, `pull-up`, `empty`的插槽
++ **`ResizeView`**: 监听节点高度变化的组件
+
+### TODO List
++ 瀑布流容器(多列)
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import RecycleList from './index.vue';
-import RecycleCore from './recycle-core.vue';
+import RecycleListCore from './recycle-core.vue';
+import RecyclePullDown from './pull-down.vue';
+import RecycleResizeView from './resize-view.vue';
 import { PULL_DOWN_STATUS } from './constants';
 
-RecycleList.Core = RecycleCore;
+RecycleList.Core = RecycleListCore;
+RecycleList.PullDown = RecyclePullDown;
+RecycleList.ResizeView = RecycleResizeView;
+
 RecycleList.PULL_DOWN_STATUS = PULL_DOWN_STATUS;
 
 export default RecycleList;
